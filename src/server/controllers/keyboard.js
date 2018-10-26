de03a58dae016c31ab185b9b500ec61917e003a9
Youngchan Kim <ovekyc@gmail.com>
2018-10-26 15:45:18 +0900

    Add keyboard

--- src/server/controllers/keyboard.js
+++ src/server/controllers/keyboard.js
@@ -1,6 +1,6 @@
 export default class TopicController {
   static get(req, res, cb) {
-    res.send({'text': 'This is hell-bot'});
+    res.send({type: 'text', text: 'This is hell-bot'});
     cb();
   }
 }
